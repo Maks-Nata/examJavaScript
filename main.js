45732c29099b710311e38d9fade7c83c09a79303
Maks-Nata <mixcim20121984@gmail.com>
2024-12-04 21:55:58 +0200

    made the list output into a text field and sorted the list by name

--- main.js
+++ main.js
@@ -1,4 +1,52 @@
-const wrapper=document.getElementsByTagName('div')[0];
-const form=document.createElement('form');
-wrapper.appendChild(form);
-form.classList.add('wrapper__form');
+
+ const input = document.getElementById('name');
+ const target = document.getElementById('target');
+ const send = document.getElementById('send');
+
+ let arrName =[];
+ let arrValue=[]
+ let ul = document.createElement('ul');
+ target.appendChild(ul);
+ send.addEventListener('click', function (e) {
+     e.preventDefault();
+     let nameValue = input.value;
+     const cleaned= nameValue.trim().split(/\s+/).join('')
+     let split=cleaned.split('=');
+     let name=split[0];
+     let value=split[1];
+     arrName.push(name);
+      arrValue.push(value)
+
+     const li= document.createElement("li");
+     ul.appendChild(li);
+     li.innerText= `${name}=${value}`;
+
+ });
+
+
+
+const buttonSortname=document.getElementById('buttonSortname');
+ const buttonSortvalue=document.getElementById('buttonSortvalue');
+const buttonDelete=document.getElementById('buttonDelete');
+buttonSortname.addEventListener("click", function (ev) {
+    ev.preventDefault()
+        let arrSortNameValue = arrName.map((name, index) => ({name: name, value: arrValue[index]}));
+
+    let arrNameSort = arrSortNameValue.sort((a,b)=>a.name.localeCompare(b.name))
+    console.log(arrNameSort);
+    ul.innerHTML = ''
+    for (const item of arrNameSort) {
+
+        let liSort = document.createElement('li');
+        ul.appendChild(liSort);
+        liSort.innerText =`${item.name}=${item.value}` ;
+    }
+
+
+
+
+})
+
+
+
+
